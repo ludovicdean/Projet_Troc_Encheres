e4633f214bb5427f9aa65e46560d390f08302505
damien <dam.rouxel@gmail.com>
2020-05-11 21:28:32 +0200

    maj utilisateurdao

--- src/fr/eni/trocEncheres/dal/UtilisateurDAO.java
+++ src/fr/eni/trocEncheres/dal/UtilisateurDAO.java
@@ -3,10 +3,14 @@ package fr.eni.trocEncheres.dal;
 import fr.eni.trocEncheres.BusinessException;
 
 public interface UtilisateurDAO {
+	//prennent un Utilisateur en parametre
 	public void ajouterUtilisateur() throws BusinessException;
 	public void modifierUtilisateur() throws BusinessException;
-	public void connecterUtilisateur() throws BusinessException;
 	public void gererUtilisateur() throws BusinessException;
-	public void supprimerUtilisateur() throws BusinessException;
-	public void getAdresseUtilisateur() throws BusinessException;
+	// retourne un Utilisateur
+	public void connecterUtilisateur(String login, String motDePasse) throws BusinessException;
+	
+	public void supprimerUtilisateur(int id) throws BusinessException;
+	//retourne un Retrait
+	public void getAdresseUtilisateur(int id) throws BusinessException;
 }
